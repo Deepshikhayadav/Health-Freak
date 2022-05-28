d904ea5d3e5d20db32c08fce62f5ef8e6a7ae765
Deepshikha yadav <deepshikhayadav2000@gmail.com>
2022-05-28 21:57:02 +0530

    google signin

--- app/src/main/java/com/deepshikhayadav/geetacollege/MainActivity.kt
+++ app/src/main/java/com/deepshikhayadav/geetacollege/MainActivity.kt
@@ -1,18 +1,130 @@
 package com.deepshikhayadav.geetacollege
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.deepshikhayadav.geetacollege.databinding.ActivityHomeBinding
+import com.deepshikhayadav.geetacollege.databinding.ActivityMainBinding
+import com.firebase.ui.auth.util.signincontainer.IdpSignInContainer.signIn
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+    private val RC_SIGN_IN=123
+    private val TAG: String = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("804335635453-8bkv4oapoaed8edqqllqm04mcmrt9jqe.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+        binding.googleLogin.setOnClickListener {
+            signIn()
+        }
 
         getTokenFcm()
     }
+
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                Toast.makeText(this, "firebaseAuthWithGoogle successful  ${account.displayName}", Toast.LENGTH_SHORT).show()
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+
+                // ...
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    startActivity(Intent(applicationContext,HomeActivity::class.java))
+                    finish()
+                   /* documentRef=db.collection("users")
+                        .document(user!!.uid)
+                    val hashMap = hashMapOf(
+                        "Email" to auth.currentUser!!.email,
+                    )
+                    documentRef.get().addOnSuccessListener {
+                        if(it.getString("admin")!=null){
+                            Toast.makeText(applicationContext,"You are admin",Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(applicationContext,AdminLogin::class.java))
+                            finish()
+                        }
+                        else{
+                            documentRef.set(hashMap)
+                            startActivity(Intent(applicationContext,Dashboard::class.java))
+                            finish()
+                        }
+                    }*/
+
+
+                } else {
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(applicationContext,"OOPs something went wrong!!",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser!=null){
+            startActivity(Intent(applicationContext,HomeActivity::class.java))
+            finish()
+
+        }
+
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
     private fun getTokenFcm() {
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener(OnCompleteListener { task ->
--- app/src/main/java/com/deepshikhayadav/geetacollege/ui/home/HomeFragment.kt
+++ app/src/main/java/com/deepshikhayadav/geetacollege/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.deepshikhayadav.geetacollege.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.deepshikhayadav.geetacollege.MainActivity
 import com.deepshikhayadav.geetacollege.databinding.FragmentHomeBinding
+import com.firebase.ui.auth.AuthUI
 import me.itangqi.waveloadingview.WaveLoadingView
 
 class HomeFragment : Fragment() {
@@ -31,8 +35,37 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        waterLevelView = binding.waterLevelView
 
+        binding.btnStats.setOnClickListener {
+            AuthUI.getInstance().signOut(requireActivity()).addOnSuccessListener {
+                Toast.makeText(activity,"Log Out",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+        initView()
         return root
+    }
+
+    private fun initView() {
+        binding.op50ml.setOnClickListener {
+            waterLevelView.progressValue=70
+        }
+        binding.op100ml.setOnClickListener {
+
+        }
+        binding.op150ml.setOnClickListener {
+
+        }
+        binding.op200ml.setOnClickListener {
+
+        }
+        binding.op250ml.setOnClickListener {
+
+        }
+        binding.opCustom.setOnClickListener {
+
+        }
     }
 
     override fun onDestroyView() {
